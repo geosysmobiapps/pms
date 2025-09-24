f82beb4711efb66bb623bb06856977d468babd75
geosysmobiapps <geosysmobiapps@gmail.com>
2025-09-24 10:19:05 +0530

    Staff and login Changes

--- accounts/views.py
+++ accounts/views.py
@@ -11,4 +11,16 @@ class LoginView(View):
         return render(request, self.template_name)
 
     def post(self, request, *args, **kwargs):
-        return redirect("master:dashboard")
+        # Get the selected account type from the form
+        account_type = request.POST.get('account_type')
+        
+        # Redirect based on account type
+        if account_type == 'admin':
+            return redirect("master:dashboard")
+        elif account_type == 'staff':
+            return redirect("staff:dashboard")
+        else:
+            # If no account type selected, redirect back to login with error
+            return render(request, self.template_name, {
+                'auth_error': 'Please select an account type'
+            })
